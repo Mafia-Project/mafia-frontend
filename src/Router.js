fae3f907ec6f8552712cc6f32b4f98c65dfeae98
WoojinDeve <jwj060111@gmail.com>
2023-07-19 14:36:26 +0900

    time-reduction

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './pages/Main'
+import GameRoom from './pages/GameRoom'
+import GameRoomWrapper from './pages/GameRoom'
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Main />} />
+        <Route path="/" element={<Main />} />
+        <Route path='/rooms/:id' element={<GameRoomWrapper />} />
       </Routes>
     </BrowserRouter>
   )
