e12c4c20806e98b5754b1cdc985e893608af93ac
WoojinDeve <jwj060111@gmail.com>
2023-07-21 21:15:06 +0900

    Feat: game

--- src/pages/GameRoom.js
+++ src/pages/GameRoom.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Client } from '@stomp/stompjs';
 import indexStore from '../store/Store';
 import GamePlayerList from '../components/Game/GamePlayerList';
