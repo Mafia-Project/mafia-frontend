6a48c4272b3073299b66143cda6c700fd740b33e
woohyeonjoe <qlqkthd8802@gmail.com>
2023-07-23 22:22:47 +0900

    방장만 게임시작

--- src/components/Game/GameStart.js
+++ src/components/Game/GameStart.js
@@ -1,17 +1,26 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { gameStartApi } from '../../public/api/axios';
 
 const GameStart = (props) => {
-    const {id} = props;
+    const {id, host} = props;
 
+    const isHostActive = host === 'true';   //값이 boolean형으로 인식이 안되서 이렇게..
+
+    const [buttonClicked, setButtonClicked] = useState(false);
+
+    //console.log('방장이냐', host);
     const onClickGameStart = () => {
-      gameStartApi(id);
+      if (!buttonClicked) {
+        gameStartApi(id);
+        setButtonClicked(true);
+      }
     };
   
     return (
       <div>
-        <Button variant="outlined" size="large" onClick={() => onClickGameStart()}>
+        {console.log('방장이냐', host)}
+        <Button variant="outlined" size="large" onClick={() => onClickGameStart()} disabled={!isHostActive || buttonClicked}>
           게임 시작
         </Button>
       </div>
